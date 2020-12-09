4f73d89bbb53a343cfff12c94a3035b4f7a4d172
HauresBogdan <hauresbogdan@gmail.com>
2020-12-09 17:34:22 +0200

    added dropshadow to main croped img

--- src/components/Home.js
+++ src/components/Home.js
@@ -68,7 +68,7 @@ function Home() {
 <section className={classNames("main2")}>
         <div className="sliding-img-parent">
           <div className="sliding-img">
-            <img src="2.png" alt="thetahealing" />
+            <img className="main-croped-img"src="2.png" alt="thetahealing" />
             <div className="mask"></div>
             <p>Reconecteaza-te cu pacea din interior!</p>
             <span className="semnatura">cu Mihaela Maierean</span>
