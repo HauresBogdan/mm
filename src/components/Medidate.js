3e26dd23b093138f1be3f7629217ccf7d742a763
HauresBogdan <hauresbogdan@gmail.com>
2020-12-17 14:48:06 +0200

    1 sec in medidate

--- src/components/Medidate.js
+++ src/components/Medidate.js
@@ -1,7 +1,35 @@
+import "../sidepagecss/medidate.css";
+import Footer from "./Footer";
+import "../sidepagecss/thetahealing.css";
+
 function Medidate() {
-    return (
-      <h1>Medi Date</h1>
-    );
-  }
-  
-  export default Medidate;
+  return (
+    <>
+      <div className="m-banner">
+        <h1 className="m-container">Medidate</h1>
+      </div>
+
+      <section className="medi1 thp-hero">
+        <div className="medi1-left">
+          <h1>Vrei sa experimentezi beneficiile meditatiei?</h1>
+          <p>Una dintre placerile mele este aceea de a medita iar MediDate a pornit din dorinta de a medita in grup.</p>
+          <p>
+            Programul iti ofera posibilitatea de a face parte dintr-un grup care se intalneste in mediul virtual pentru a medita. Scopul acestei practici este
+            de a crea un spatiu in care tu sa poti sa iti linistesti mintea si in care sa iti setezi intentia de a fi centrat in momentul prezent.
+          </p>
+          <p>*Este GRATUIT</p>
+          <div className="center">
+            <button className="violet-btn mt1">Programează o ședință</button>
+          </div>
+        </div>
+        <div className="medi1-right">
+                <img src="m1.jpg" alt="mihaela maierean thetahealing" />
+              </div>
+      </section>
+
+     
+    </>
+  );
+}
+
+export default Medidate;
